28d12cbf8e8f0d3a21ef37205a0ddbf5c891f797
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-06-14 02:54:30 -0700

    added fonts and update app.json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function HomeScreen() {
+ 
   return (
-    <View >
-      <Text className="text-3xl text-center pt-10 text-rose-600">Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl text-center pt-10 font-pextrabold">Aura</Text>
       <StatusBar style="auto" />
     </View>   
   );
