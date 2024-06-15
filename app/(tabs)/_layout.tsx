f0e46f6e8f8419e4e244a2ea9a42ab32192741c5
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-06-15 01:22:00 -0700

    "Updated tabBar styles in TabsLayout component"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,6 +26,14 @@ const TabsLayout = () => {
     <Tabs
       screenOptions={{
         tabBarShowLabel: false,
+        tabBarActiveTintColor:'#FFA001',
+        tabBarInactiveTintColor:'#CDCDE0',
+        tabBarStyle:{
+          backgroundColor:'#161622',
+          borderTopWidth:1,
+          borderTopColor: '#232533',
+          height: 84
+        }
       }}
     >
       <Tabs.Screen
